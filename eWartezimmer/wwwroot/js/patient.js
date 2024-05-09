dd8152bb06bb7493c926367e45bbd49003f83408
Bastian Kröger <bastian.kroeger@pm.me>
2024-05-09 14:24:13 +0200

    fixed patient name in Patient view

--- eWartezimmer/wwwroot/js/patient.js
+++ eWartezimmer/wwwroot/js/patient.js
@@ -5,6 +5,8 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/eWartezimmerHub").
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
+var user = null;
+
 var map = L.map('map').setView([51.934328,7.651021], 15.5);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -14,6 +16,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Set the initial value of the variable
 let guid = document.getElementById("guid").value;
+var greeting = document.getElementById("greeting").value;
 
 var circle = L.circle([51.934328,7.651021], {
     color: '#01c7b2',
@@ -31,58 +34,31 @@ connection.on("ReceiveMessage", function (user, message) {
     li.textContent = `${user} says ${message}`;
 });
 
+connection.on("AllQueuers", (jsonListOfQueuers) => {
+    console.log("all queuers");
+    // Parse the JSON list of patients
+    const patients = JSON.parse(jsonListOfQueuers);
+    const yourPatient = patients.find(patient => patient.Guid === guid);
+    if (yourPatient) {
+        user = yourPatient;
+        console.log(yourPatient.WaitingTime);
+        circle.setRadius(yourPatient.WaitingTime); // Update the circle radius
+        map.fitBounds(circle.getBounds());
+        document.getElementById("greeting").innerHTML = "Hallo, " + yourPatient.Name + ".";
+        document.getElementById("countdown").innerHTML = "Sie haben noch " + ((Math.floor(yourPatient.WaitingTime / 60) > 0)? Math.floor(yourPatient.WaitingTime / 60) + " Minuten und " : "") + yourPatient.WaitingTime % 60 + " Sekunden Zeit, bis Sie an der Reihe sind.";
+    }
+  });
+
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
-// Function to update the countdown variable and display it
-//function updateCountdown() {
-//  // Decrement the countdown value
-//
-//  // Display the countdown value
-//  document.getElementById("countdown").innerHTML = "Sie haben noch " + ((Math.floor(countdownValue / 60) > 0)? Math.floor(countdownValue / 60) + " Minuten und " : "") + countdownValue % 60 + " Sekunden Zeit, bis Sie an der Reihe sind.";
-//  
-//  circle.setRadius(countdownValue)
-//
-//  // Check if the countdown has reached zero
-//  if (countdownValue <= 0) {
-//    clearInterval(interval); // Stop the countdown
-//    document.getElementById("countdown").innerHTML = "Countdown finished!";
-//  }
-//}
-
-connection.on("AllQueuers", (jsonListOfQueuers) => {
-  console.log("all queuers");
-  // Parse the JSON list of patients
-  const patients = JSON.parse(jsonListOfQueuers);
-
-
-  
-
-  const yourPatient = patients.find(patient => patient.Guid === guid);
-  if (yourPatient) {
-      console.log(yourPatient.WaitingTime);
-      circle.setRadius(yourPatient.WaitingTime); // Update the circle radius
-      map.fitBounds(circle.getBounds());
-      document.getElementById("countdown").innerHTML = "Sie haben noch " + ((Math.floor(yourPatient.WaitingTime / 60) > 0)? Math.floor(yourPatient.WaitingTime / 60) + " Minuten und " : "") + yourPatient.WaitingTime % 60 + " Sekunden Zeit, bis Sie an der Reihe sind.";
-  }
-});
-
 document.getElementById("sendButton").addEventListener("click", function (event) {
-  var user = document.getElementById("userInput").value;
   var message = document.getElementById("messageInput").value;
-  connection.invoke("SendMessage", user, message).catch(function (err) {
+  connection.invoke("SendMessage", user.Name, message).catch(function (err) {
       return console.error(err.toString());
   });
   event.preventDefault();
 });
-
-
-
-
-
-// Call the updateCountdown function every second
-// const interval = setInterval(updateCountdown, 1000);
-
